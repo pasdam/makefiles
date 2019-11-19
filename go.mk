15dc64a46cc320d0ccb1807b87464202af59d669
pasdam <pasdam@users.noreply.github.com>
2019-11-19 20:35:58 +0800

    Disable inlining for go tests

--- go.mk
+++ go.mk
@@ -11,15 +11,15 @@ go-clean:
 ## go-coverage: Generate global code coverage report
 .PHONY: go-coverage
 go-coverage: | __go-pkg-list
-	@go test -v ${GO_PKG_LIST} -coverprofile /tmp/pls_cp.out || true
+	@go test -gcflags=-l -v ${GO_PKG_LIST} -coverprofile /tmp/pls_cp.out || true
 	@go tool cover -html=/tmp/pls_cp.out -o /tmp/coverage.html
 	@echo "You can find coverage report at /tmp/coverage.html"
 
 ## go-check: Run linter, perform unit tests, and verify the coverage
 .PHONY: go-check
 go-check: | __go-pkg-list go-lint go-coverage
-	@go test -v -race -short ${GO_PKG_LIST}
-	@go test -v -msan -short ${GO_PKG_LIST}
+	@go test -gcflags=-l -v -race -short ${GO_PKG_LIST}
+	@go test -gcflags=-l -v -msan -short ${GO_PKG_LIST}
 
 ## go-lint: Lint the files
 .PHONY: go-lint
@@ -34,7 +34,7 @@ go-run:
 ## go-test: Run unit tests
 .PHONY: go-test
 go-test: | __go-pkg-list
-	@go test ${GO_PKG_LIST}
+	@go test -gcflags=-l ${GO_PKG_LIST}
 
 __go-pkg-list:
 ifeq ($(origin GO_PKG_LIST), undefined)
