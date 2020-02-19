d4358b6ed93be63e699cafbc52ae95c1c74c3cc9
pasdam <pasdam@users.noreply.github.com>
2020-02-19 08:59:32 +0800

    Create DOCKER_IMAGE_TAG variable only when needed, to avoid to run git status command all the time

--- docker.mk
+++ docker.mk
@@ -7,23 +7,14 @@
 # repository, as the tag will be the sha of the latest commit, plus the suffix
 # ".dirty" if the repository contains uncommitted files.
 
-PROJECT_NAME ?= $(shell basename $(shell pwd))
-
-ifndef DOCKER_IMAGE_TAG
-__GIT_UNCOMMITTED_FILES := $(shell git status -s)
-ifeq (__GIT_UNCOMMITTED_FILES, "")
-	DOCKER_IMAGE_TAG ?= $(shell git rev-parse --short HEAD 2> /dev/null)
-else
-	DOCKER_IMAGE_TAG ?= $(shell git rev-parse --short HEAD 2> /dev/null).dirty
-endif
-endif
+PROJECT_NAME ?= $(shell basename $(dir $(abspath $(firstword $(MAKEFILE_LIST)))))
 
 ## docker-build: Build the docker image with the tag
 ##               <PROJECT_NAME>:<DOCKER_IMAGE_TAG>, if DOCKER_IMAGE_TAG is not
 ##               defined it will be the git revision, plus ".dirty" suffix if
 ##               the repository contains uncommitted files.
 .PHONY: docker-build
-docker-build:
+docker-build: | docker-generate-tag
 ifneq (__GIT_UNCOMMITTED_FILES, "")
 	@echo "\033[33mThe repository contains local changes, this image should only be used for testing\033[0m";
 endif
@@ -36,7 +27,7 @@ endif
 ##               This command WON'T delete any intermediate images, so those
 ##               should be manually removed.
 .PHONY: docker-clean
-docker-clean:
+docker-clean: | docker-generate-tag
 	@docker rmi $(PROJECT_NAME):$(DOCKER_IMAGE_TAG) || true
 
 ## docker-run: Run a docker container from the image with the tag
@@ -45,5 +36,15 @@ docker-clean:
 ##             repository contains uncommitted files. The container will be
 ##             automatically removed once stopped.
 .PHONY: docker-run
-docker-run: | docker-build
+docker-run: | docker-generate-tag docker-build
 	@docker run -it --rm $(PROJECT_NAME):$(DOCKER_IMAGE_TAG)
+
+docker-generate-tag:
+	@$(eval __GIT_UNCOMMITTED_FILES := $(shell git status -s))
+	@$(eval DOCKER_IMAGE_TAG := $(shell git rev-parse --short HEAD 2> /dev/null))
+
+	@if [ ! -z "$(__GIT_UNCOMMITTED_FILES)" ]; then \
+		echo "The repo has uncommitted files"; \
+		$(eval DOCKER_IMAGE_TAG := $(DOCKER_IMAGE_TAG).dirty) \
+	fi;
+	@echo "Image tag: "$(DOCKER_IMAGE_TAG)
