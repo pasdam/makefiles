cd756d9b5a202f2f75808155de23ddf82b3edad3
pasdam <pasdam@users.noreply.github.com>
2023-10-10 20:51:06 +0800

    feat(compose): update targets to avoid order only prerequisites to be invoked all the time

--- docker-compose.mk
+++ docker-compose.mk
@@ -16,9 +16,9 @@ compose-down:
 ##                               container re-creation
 .PHONY: compose-generate-config-tags
 compose-generate-config-tags:
+	@1>&2 echo "Generating docker-compose with last modified tags"
 	@set -e; \
-		deps=$$(echo $(COMPOSE_FILES_ARGS) | sed 's|-f ||g'); \
-		merged_compose=$$(yq eval-all '. as $$item ireduce ({}; . * $$item )' $$deps); \
+		merged_compose=$$(yq eval-all '. as $$item ireduce ({}; . * $$item )' $(COMPOSE_FILES)); \
 		services=$$(printf "%s" "$$merged_compose" | yq '.services | keys | .[]'); \
 		for service in $$services; do \
 			volumes=$$(printf "%s" "$$merged_compose" | yq -r ".services.$${service}.volumes[]"); \
@@ -34,23 +34,28 @@ compose-generate-config-tags:
 			done <<< "$$volumes" ; \
 			output=$$(printf "%s" "$$output" | yq '.services.'"$$service"'.labels."com.pasdam.volumes-last-modified-timestamp" |= '$$lastModificationSeconds''); \
 		done; \
-		printf "%s" "$$output" > $(COMPOSE_TAGS_LOCAL_YAML);
+		printf "%s" "$$output" > $(COMPOSE_TAGS_LOCAL_YAML)
 
 ## compose-generate-config-tags-target: Generate a makefile with a target to
 ##                                      calculate the last-modified-timestamp
 ##                                      tag for each container
+.PHONY: compose-generate-config-tags-target
 compose-generate-config-tags-target:
 	@deps=$$(cat $(COMPOSE_FILES) | grep -E '\- \./.*:ro$$' | sed -E 's| *\- \./||' | sed 's|:.*||' | tr '\n' ' ') && \
-		printf '$(COMPOSE_TAGS_LOCAL_YAML): %s | compose-generate-config-tags\n' "$$deps" > $(COMPOSE_LOCAL_MK)
+		printf '$(COMPOSE_TAGS_LOCAL_YAML): %s\n	@$$(MAKE) compose-generate-config-tags\n' "$$deps" > $(COMPOSE_LOCAL_MK)
 
 ## compose-up: Start the docker compose environment
 .PHONY: compose-up
-compose-up: ${COMPOSE_TAGS_LOCAL_YAML} $(COMPOSE_LOCAL_MK)
+compose-up: ${COMPOSE_TAGS_LOCAL_YAML}
 	@docker compose $(COMPOSE_FILES_ARGS) -f $(COMPOSE_TAGS_LOCAL_YAML) up $(COMPOSE_UP_ARGS)
 
 # Internal targets
 # ================
 
+${COMPOSE_TAGS_LOCAL_YAML}: $(COMPOSE_LOCAL_MK)
+
 # Generate the makefile with the target to calculate the last-modified-timestamp
 # tag for each container
-$(COMPOSE_LOCAL_MK): $(COMPOSE_FILES) | compose-generate-config-tags-target
+$(COMPOSE_LOCAL_MK): $(COMPOSE_FILES)
+	@1>&2 echo "Generating $(@)"
+	@$(MAKE) compose-generate-config-tags-target
