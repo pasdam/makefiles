6d5648e5d18138093186ee46d311fb826a896368
pasdam <pasdam@users.noreply.github.com>
2023-10-13 20:59:57 +0800

    refactor(compose): rename variables, and simplify the compose files list argument

--- docker-compose.mk
+++ docker-compose.mk
@@ -1,9 +1,10 @@
-COMPOSE_TAGS_LOCAL_YAML ?= compose.local.yaml
-COMPOSE_FILES_ARGS ?= -f compose.yaml
+COMPOSE_FILES ?= compose.yaml
+COMPOSE_LAST_MODIFIED_TAGS_YAML ?= compose.last-modified-tags.yaml
 COMPOSE_UP_ARGS ?= -d
+COMPOSE_UP_PREREQUISITES ?=
+COMPOSE_VOLUME_FILES ?= $(shell cat $(COMPOSE_FILES) | grep -E '\- \./.*:ro$$' | sed -E 's| *\- \./||' | sed 's|:.*||' | sort | uniq | tr '\n' ' ')
 
-COMPOSE_FILES := $(shell echo $(COMPOSE_FILES_ARGS) | sed 's|-f ||g')
-COMPOSE_VOLUME_FILES := $(shell cat $(COMPOSE_FILES) | grep -E '\- \./.*:ro$$' | sed -E 's| *\- \./||' | sed 's|:.*||' | sort | uniq | tr '\n' ' ')
+COMPOSE_FILES_ARGS := $(shell echo " $(COMPOSE_FILES)" | sed 's| | -f |g')
 
 ## compose-down: Stop the docker compose environment
 .PHONY: compose-down
@@ -15,17 +16,17 @@ compose-down:
 ##                               modified read-only volume, to force the
 ##                               container re-creation
 .PHONY: compose-generate-config-tags
-compose-generate-config-tags: $(COMPOSE_TAGS_LOCAL_YAML)
+compose-generate-config-tags: $(COMPOSE_LAST_MODIFIED_TAGS_YAML)
 
 ## compose-up: Start the docker compose environment
 .PHONY: compose-up
-compose-up: ${COMPOSE_TAGS_LOCAL_YAML}
-	@docker compose $(COMPOSE_FILES_ARGS) -f $(COMPOSE_TAGS_LOCAL_YAML) up $(COMPOSE_UP_ARGS)
+compose-up: $(COMPOSE_LAST_MODIFIED_TAGS_YAML) $(COMPOSE_VOLUME_FILES) $(COMPOSE_UP_PREREQUISITES)
+	@docker compose $(COMPOSE_FILES_ARGS) -f $(COMPOSE_LAST_MODIFIED_TAGS_YAML) up $(COMPOSE_UP_ARGS)
 
 # Internal targets
 # ================
 
-$(COMPOSE_TAGS_LOCAL_YAML): $(COMPOSE_VOLUME_FILES)
+$(COMPOSE_LAST_MODIFIED_TAGS_YAML): $(COMPOSE_VOLUME_FILES)
 	@1>&2 echo "Generating $@ because $? have changed"
 	@set -e; \
 		merged_compose=$$(yq eval-all '. as $$item ireduce ({}; . * $$item )' $(COMPOSE_FILES)); \
@@ -44,4 +45,4 @@ $(COMPOSE_TAGS_LOCAL_YAML): $(COMPOSE_VOLUME_FILES)
 			done <<< "$$volumes" ; \
 			output=$$(printf '%s' "$$output" | yq ".services.$$service.labels.\"com.pasdam.volumes-last-modified-timestamp\" |= $$lastModificationSeconds"); \
 		done; \
-		printf '%s' "$$output" > $(COMPOSE_TAGS_LOCAL_YAML)
+		printf '%s' "$$output" > $@
